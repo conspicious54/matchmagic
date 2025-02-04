43c3af70a29e3b26da887765c7ffc015d5f69a5b
conspicious54 <connor.wbyers@gmail.com>
2025-02-03 21:40:39 -0500

    Updated 8 files [BoltSync]

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow external connections
-    port: parseInt(process.env.PORT || '8080'), // Use Google Cloud's port or default to 8080
-    strictPort: true // Fail if port is in use
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '8080'),
+    strictPort: true
   },
   preview: {
     host: '0.0.0.0',
